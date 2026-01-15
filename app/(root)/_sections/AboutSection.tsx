ac97f7b85cc647359b50e1e67a24606414915c1b
Adrian <adriank@gmx.de>
2026-01-15 19:10:54 +0100

    changed logo, addes social media

--- app/(root)/_sections/AboutSection.tsx
+++ app/(root)/_sections/AboutSection.tsx
@@ -5,8 +5,8 @@ const AboutSection = () => {
   return (
     <section id="about" className="my-15 w-[90%] mx-auto">
       <Headline title="Über Tabtique" />
-      <div className="grid grid-cols-1 md:grid-cols-4 mt-8">
-        <div className="col-span-3">
+      <div className="grid grid-cols-1 md:grid-cols-2 mt-8 w-[90%] md:max-w-300 mx-auto">
+        <div>
           <Image
             src="/profile.jpg"
             alt="Tabtique"
--- app/Footer.tsx
+++ app/Footer.tsx
@@ -1,6 +1,7 @@
 import Logo from '@/public/Logo';
 import TabiqueLogo from '@/public/TabiqueLogo';
 import Link from 'next/link';
+import { SiInstagram, SiFacebook, SiX } from '@icons-pack/react-simple-icons';
 
 const Footer = () => {
   return (
@@ -8,6 +9,19 @@ const Footer = () => {
       <Logo className="w-10 mb-7" />
       <TabiqueLogo className="fill-white w-80 mb-15" />
       <p>Hochstraße 43, 60313 Frankfurt am Main</p>
+      <div className="flex gap-14 mt-15">
+        <Link
+          href="https://www.instagram.com/tabtique?igsh=a3psdDUxdWY0azg%3D&utm_source=qr"
+          aria-label="Instagram">
+          <SiInstagram className="h-7 w-7 text-white" />
+        </Link>
+        <Link href="#" aria-label="Facebook">
+          <SiFacebook className="h-7 w-7 text-white" />
+        </Link>
+        <Link href="#" aria-label="X">
+          <SiX className="h-7 w-7 text-white" />
+        </Link>
+      </div>
       <div className="mt-15">
         <Link href="/imprint">Impressum</Link>
       </div>
