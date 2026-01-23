eac441e35ebb25ebc058e8696837377b82dc21a5
Adrian <adriank@gmx.de>
2026-01-24 00:01:31 +0100

    treatment icons

--- app/[locale]/(root)/_sections/Contact.tsx
+++ app/[locale]/(root)/_sections/Contact.tsx
@@ -7,12 +7,21 @@ import Headline from '@/components/Headline';
 import Button from '@/components/Button';
 import Image from 'next/image';
 import Link from 'next/link';
+import MicroneedlingIcon from '@/public/MicroneedlingIcon';
+import SignatureIcon from '@/public/SignatureIcon';
+import AquafacialIcon from '@/public/AquafacialIcon';
+import UltimateIcon from '@/public/UltimateIcon';
+import { log } from 'console';
+import { cn } from '@/lib/utils';
 
 const Contact = () => {
   const t = useTranslations('contact');
   const [status, setStatus] = useState<
     'idle' | 'loading' | 'error' | 'success'
   >('idle');
+  const [treatment, setTreatment] = useState<
+    'signature' | 'microneedling' | 'aquafacial' | 'ultimate' | null
+  >(null);
   const [error, setError] = useState<string | null>(null);
 
   async function onSubmit(e: React.FormEvent<HTMLFormElement>) {
@@ -63,6 +72,44 @@ const Contact = () => {
       <p className="p-5 leading-7 mt-7 font-light md:text-center">
         {t('description')}
       </p>
+      <div className="flex justify-between mb-8 w-70 mx-auto">
+        <SignatureIcon
+          className={cn(
+            'w-10 h-10 cursor-pointer transition-[fill] duration-700',
+            treatment === 'signature' ? 'fill-gray-700' : 'fill-gray-300',
+          )}
+          onClick={() => {
+            setTreatment('signature');
+          }}
+        />
+        <AquafacialIcon
+          className={cn(
+            'w-10 h-10 cursor-pointer transition-[fill] duration-700',
+            treatment === 'aquafacial' ? 'fill-gray-700' : 'fill-gray-300',
+          )}
+          onClick={() => {
+            setTreatment('aquafacial');
+          }}
+        />
+        <MicroneedlingIcon
+          className={cn(
+            'w-10 h-10 cursor-pointer transition-[fill] duration-700',
+            treatment === 'microneedling' ? 'fill-gray-700' : 'fill-gray-300',
+          )}
+          onClick={() => {
+            setTreatment('microneedling');
+          }}
+        />
+        <UltimateIcon
+          className={cn(
+            'w-10 h-10 cursor-pointer transition-[fill] duration-700',
+            treatment === 'ultimate' ? 'fill-gray-700' : 'fill-gray-300',
+          )}
+          onClick={() => {
+            setTreatment('ultimate');
+          }}
+        />
+      </div>
       <div className="my-15 grid grid-cols-1 md:grid-cols-2 gap-12">
         <div className="self-center">
           <form
